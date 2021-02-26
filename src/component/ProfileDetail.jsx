d4e92863bdd5a3b53cc8dbe7048638396e990792
Gökay Abay <abaygokay@gmail.com>
2021-02-25 17:44:48 -0800

    #9 display fname lname on profiledetail screen

--- src/component/ProfileDetail.jsx
+++ src/component/ProfileDetail.jsx
@@ -1,9 +1,25 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import { getOne } from "../network"
 
 const ProfileDetail = () => {
+  const { id } = useParams()
+
+  const [profile, setProfile] = useState()
+
+  useEffect(() => {
+    ;(async () => {
+      const res = await getOne(id)
+      console.log(res)
+      setProfile(res[0])
+    })()
+  }, [])
+
   return (
     <div>
-      <h1>Test</h1>
+      <p>
+        {profile?.fname} {profile?.lname}
+      </p>
     </div>
   )
 }
